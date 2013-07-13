3f1b18171493183f12896c5151e9fea16fabc957
Cagil Tasdemir <cagiltasdemir@gmail.com>
2013-07-13 03:03:29 +0000

    updated again

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+fs.readFileSync('index.html', function(err,data){
+if (err) throw err;
+console.log(data);
+});
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(data.toString);
 });
 
 var port = process.env.PORT || 5000;
